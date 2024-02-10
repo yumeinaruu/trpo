b620ddd90e89efe29f22fc35a3ed7fb99933d61c
Yumeinaruu <lisovoistas@yandex.ru>
2024-02-10 12:54:56 +0300

    Initial commit

---
+++ src/main/java/task1/Main.java
@@ -0,0 +1,23 @@
+package task1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numbers from 1 to 100: ");
+        String[] numbers = scanner.nextLine().split(" ");
+        int[] numArray = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++){
+            numArray[i] = Integer.parseInt(numbers[i]);
+        }
+
+        Arrays.stream(numArray).filter((value -> value % 3 == 0 && value < 100 && value > 0)).forEach(System.out::println);
+
+        System.out.println("Сделал студент группы 222403 Лисовой Станислав.");
+        System.out.println("Задание принято: 10.02.2024");
+        System.out.println("Задание сдано: 10.02.2024");
+    }
+}
